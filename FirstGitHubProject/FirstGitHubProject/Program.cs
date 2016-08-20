926dbaf68e7d3b6aa6db1aec06344043d6e7936d
Tim <blueshift@stny.rr.com>
2016-08-20 07:01:20 -0400

    My first Git commit

---
+++ FirstGitHubProject/FirstGitHubProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FirstGitHubProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Horrible string";
+
+            Console.WriteLine("This is a {0}", s);
+            Console.Read();
+        }
+    }
+}
